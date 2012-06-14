ce93fc3a7fd1f06e46daafbefe204eec6414f879
Michael Roop <michael@roop.ca>
2012-06-14 15:58:00 -0300

    Added log delegates to wrap to ExceptionFault methods
    To trap all exceptions and message them to ExceptionFaults while
    preserving error location and other data. Provide log delegates to the
    error can be logged within the component

--- ChkUtils/WrapActionToErrFaultException.cs.cs
+++ ChkUtils/WrapActionToErrFaultException.cs.cs
@@ -13,6 +13,13 @@ namespace ChkUtils {
     /// <summary>
     /// Partial class implementation with catch action to ErrReportFaultException
     /// </summary>
+    /// <remarks>
+    /// There are signatures that will allow a logging delegate to be fired on error. These are
+    /// to be used at 'entry points' to a WCF service class. Any exception that occurs is caught 
+    /// at this level and transformed to an ExceptionFault that can safely traverse WCF boundries.
+    /// The log delegate allows the error to be logged in the component before the Exception
+    /// fault is sent out accross the wire.
+    /// </remarks>
     /// <author>Michael Roop</author>
     public static partial class WrapErr {
 
@@ -26,11 +33,19 @@ namespace ChkUtils {
         /// <param name="msg">The error message</param>
         /// <param name="action">The action to invoke</param>
         public static void ToErrorReportFaultException(int code, string msg, Action action) {
-            WrapErr.ToErrorReportFaultException(code, msg, (report) => { /* no logging */ }, action);
+            WrapErr.ToLoggedErrorReportFaultException(code, msg, (report) => { /* no logging */ }, action);
         }
 
 
-        public static void ToErrorReportFaultException(int code, string msg, LogingMsgDelegate logAction, Action action) {
+        /// <summary>
+        /// Wrap an action to catch and convert exceptions not previously caught and 
+        /// converted to FaultException<ErrReport> to traverse WCF boundries. Invoke log action
+        /// </summary>
+        /// <param name="code">The error code</param>
+        /// <param name="msg">The error message</param>
+        /// <param name="logAction">The log action to invoke on error</param>
+        /// <param name="action">The action to invoke</param>
+        public static void ToLoggedErrorReportFaultException(int code, string msg, LogingMsgDelegate logAction, Action action) {
             try {
                 action.Invoke();
             }
@@ -43,18 +58,11 @@ namespace ChkUtils {
                 throw new FaultException<ErrReport>(e.Report);
             }
             catch (Exception e) {
-                WrapErr.SafeAction(() => {
-                    ErrReport err = WrapErr.GetErrReport(code, msg, e);
-                    WrapErr.SafeAction(() => logAction.Invoke(err));
-                    throw new FaultException<ErrReport>(err);
-                });
-
-                throw new FaultException<ErrReport>(WrapErr.GetErrReport(code, msg, e));
+                ErrReport err = WrapErr.GetErrReport(code, msg, e);
+                WrapErr.SafeAction(() => logAction.Invoke(err));
+                throw new FaultException<ErrReport>(err);
             }
         }
-
-
-
 
 
         /// <summary>
@@ -66,20 +74,39 @@ namespace ChkUtils {
         /// <param name="errMsgFunc">The error message on error function</param>
         /// <param name="action">The action to invoke</param>
         public static void ToErrorReportFaultException(int code, Func<string> errMsgFunc, Action action) {
+            WrapErr.ToLoggedErrorReportFaultException(code, errMsgFunc, (report) => { /* No logging */ }, action);
+        }
+
+
+        /// <summary>
+        /// Wrap an action to catch and convert exceptions not previously caught and converted to 
+        /// FaultException<ErrReport> to traverse WCF boundries. Efficient form where error 
+        /// message formating Func is not invoked unless there is an error. Includes a log delegate
+        /// to push log message before sending over WCF connection
+        /// </summary>
+        /// <param name="code">The error code</param>
+        /// <param name="errMsgFunc">The error message on error function</param>
+        /// <param name="logAction">The log action to invoke on error</param>
+        /// <param name="action">The action to invoke</param>
+        public static void ToLoggedErrorReportFaultException(int code, Func<string> errMsgFunc, LogingMsgDelegate logAction, Action action) {
             try {
                 action.Invoke();
             }
-            catch (FaultException<ErrReport>) {
+            catch (FaultException<ErrReport> e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Detail));
                 throw;
             }
             catch (ErrReportException e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Report));
                 throw new FaultException<ErrReport>(e.Report);
             }
             catch (Exception e) {
-                throw new FaultException<ErrReport>(WrapErr.GetErrReport(code, WrapErr.SafeAction(errMsgFunc), e));
+                ErrReport err = WrapErr.GetErrReport(code, WrapErr.SafeAction(errMsgFunc), e);
+                WrapErr.SafeAction(() => logAction.Invoke(err));
+                throw new FaultException<ErrReport>(err);
             }
         }
-
+        
         #endregion
 
         #region Action and Finally
@@ -92,18 +119,37 @@ namespace ChkUtils {
         /// <param name="code">The error code</param>
         /// <param name="msg">The error message</param>
         /// <param name="action">The action to invoke</param>
+        /// <param name="finallyAction">The finally action</param>
         public static void ToErrorReportFaultException(int code, string msg, Action action, Action finallyAction) {
+            WrapErr.ToLoggedErrorReportFaultException(code, msg, (report) => { /* no logging */ }, action, finallyAction);
+        }
+
+
+        /// <summary>
+        /// Wrap an action to catch and convert exceptions not previously caught and 
+        /// converted to FaultException<ErrReport> to traverse WCF boundries. Invoke log action
+        /// </summary>
+        /// <param name="code">The error code</param>
+        /// <param name="msg">The error message</param>
+        /// <param name="logAction">The log action to invoke on error</param>
+        /// <param name="action">The action to invoke</param>
+        /// <param name="finallyAction">The finally action</param>
+        public static void ToLoggedErrorReportFaultException(int code, string msg, LogingMsgDelegate logAction, Action action, Action finallyAction) {
             try {
                 action.Invoke();
             }
-            catch (FaultException<ErrReport>) {
+            catch (FaultException<ErrReport> e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Detail));
                 throw;
             }
             catch (ErrReportException e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Report));
                 throw new FaultException<ErrReport>(e.Report);
             }
             catch (Exception e) {
-                throw new FaultException<ErrReport>(WrapErr.GetErrReport(code, msg, e));
+                ErrReport err = WrapErr.GetErrReport(code, msg, e);
+                WrapErr.SafeAction(() => logAction.Invoke(err));
+                throw new FaultException<ErrReport>(err);
             }
             finally {
                 WrapErr.SafeAction(finallyAction);
@@ -122,17 +168,37 @@ namespace ChkUtils {
         /// <param name="action">The action to invoke</param>
         /// <param name="finallyAction">The finally action to invoke</param>
         public static void ToErrorReportFaultException(int code, Func<string> errMsgFunc, Action action, Action finallyAction) {
+            WrapErr.ToLoggedErrorReportFaultException(code, errMsgFunc, (report) => { /* No logging */ }, action, finallyAction);
+        }
+
+
+        /// <summary>
+        /// Wrap an action to catch and convert exceptions not previously caught and converted to 
+        /// FaultException<ErrReport> to traverse WCF boundries. Efficient form where error 
+        /// message formating Func is not invoked unless there is an error. Includes a log delegate
+        /// to push log message before sending over WCF connection
+        /// </summary>
+        /// <param name="code">The error code</param>
+        /// <param name="errMsgFunc">The error message on error function</param>
+        /// <param name="logAction">The log action to invoke on error</param>
+        /// <param name="action">The action to invoke</param>
+        /// <param name="finallyAction">The finally action</param>
+        public static void ToLoggedErrorReportFaultException(int code, Func<string> errMsgFunc, LogingMsgDelegate logAction, Action action, Action finallyAction) {
             try {
                 action.Invoke();
             }
-            catch (FaultException<ErrReport>) {
+            catch (FaultException<ErrReport> e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Detail));
                 throw;
             }
             catch (ErrReportException e) {
+                WrapErr.SafeAction(() => logAction.Invoke(e.Report));
                 throw new FaultException<ErrReport>(e.Report);
             }
             catch (Exception e) {
-                throw new FaultException<ErrReport>(WrapErr.GetErrReport(code, WrapErr.SafeAction(errMsgFunc), e));
+                ErrReport err = WrapErr.GetErrReport(code, WrapErr.SafeAction(errMsgFunc), e);
+                WrapErr.SafeAction(() => logAction.Invoke(err));
+                throw new FaultException<ErrReport>(err);
             }
             finally {
                 WrapErr.SafeAction(finallyAction);
--- TestCases/ChkUtilsTests/ToErrorReportFaultExceptionTests.cs
+++ TestCases/ChkUtilsTests/ToErrorReportFaultExceptionTests.cs
@@ -12,65 +12,90 @@ namespace TestCases.ChkUtilsTests {
     [TestFixture]
     public class ToErrorReportFaultExceptionTests {
 
+        #region Data
 
-        private void ExceptionThrowTest() {
-            Console.WriteLine("Throwing exception from method");
+        private bool logged = false;
 
-            throw new Exception("Blah blah blah");
+        private bool msgFormated = false;
+
+        #endregion
+
+        #region Setup
+        
+        [SetUp]
+        public void SetupTest() {
+            this.logged = false;
+            this.msgFormated = false;
         }
 
+        #endregion
+
+        #region Logging delegate tests
 
         [Test]
-        public void FaultExceptionLogingDelegateTest() {
-
-            bool logged = false;
-            ChkUtils.LogingMsgDelegate logDelegate = new ChkUtils.LogingMsgDelegate((report) => {
-                logged = true;
-            });
-
-
+        public void FaultException_LogingDelegateTest() {
             try {
-                //WrapErr.ToErrorReportFaultException(12345, "Big Booboo", (errReport) => { logged = true; }, () => {
-                WrapErr.ToErrorReportFaultException(12345, "Big Booboo", logDelegate, () => {
-                    this.ExceptionThrowTest();
+                WrapErr.ToLoggedErrorReportFaultException(12345, "Big Booboo", this.LogDelegate, () => {
+                    this.ExceptionThrowTest("Blah blah blah");
                 });
-
-                Console.WriteLine("After wrap");
-
             }
             catch (FaultException<ErrReport> e) {
-
-                Console.WriteLine("In catch");
-
-
                 ErrReport err = e.Detail;
                 Assert.AreEqual(12345, err.Code);
                 Assert.AreEqual("ToErrorReportFaultExceptionTests", err.AtClass);
-                Assert.AreEqual("FaultExceptionLogingDelegateTest", err.AtMethod);
+                Assert.AreEqual("FaultException_LogingDelegateTest", err.AtMethod);
                 Assert.AreEqual("Big Booboo", err.Msg);
-                Assert.IsTrue(logged, "The logging delegate did not get invoked");
-
+                Assert.IsTrue(this.logged, "The logging delegate did not get invoked");
                 Assert.IsTrue(err.StackTrace.Contains("ToErrorReportFaultExceptionTests"));
                 Assert.IsTrue(err.StackTrace.Contains("Blah blah blah"));
-
-
                 return;
             }
-
-
             Assert.Fail("Should have caught exception and not gotten here");
+        }
+
+        [Test]
+        public void FaultException_LogingAndFormatingDelegateTest() {
+            try {
+                WrapErr.ToLoggedErrorReportFaultException(9999,
+                    () => { this.msgFormated = true; return "Oops"; }, 
+                    this.LogDelegate, 
+                    () => { this.ExceptionThrowTest("It Blew!"); });
+            }
+            catch (FaultException<ErrReport> e) {
+                ErrReport err = e.Detail;
+                Assert.AreEqual(9999, err.Code);
+                Assert.AreEqual("ToErrorReportFaultExceptionTests", err.AtClass);
+                Assert.AreEqual("FaultException_LogingAndFormatingDelegateTest", err.AtMethod);
+                Assert.AreEqual("Oops", err.Msg);
+                Assert.IsTrue(this.msgFormated, "The message formating delegate did not get invoked");
+                Assert.IsTrue(err.StackTrace.Contains("ToErrorReportFaultExceptionTests"));
+                Assert.IsTrue(err.StackTrace.Contains("It Blew!"));
+                return;
+            }
+            Assert.Fail("Should have caught exception and not gotten here");
+        }
+
+        #endregion
 
 
-            //ErrReport err;
-            //WrapErr.ToErrReport(out err, 1111, () => { return "The error formating has been invoked"; }, () => {
-            //    new ClassTwo().DoIt();
-            //});
 
 
+        #region Private Methods
 
+        /// <summary>
+        /// A method which throws an exception
+        /// </summary>
+        private void ExceptionThrowTest(string msg) {
+            Console.WriteLine("Throwing exception from method");
+            throw new Exception(msg);
         }
 
 
+        private void LogDelegate(ErrReport report) {
+            this.logged = true;
+        }
+
+        #endregion
 
 
     }
