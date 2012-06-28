bad3db4d5514cbc48f60c6a65149e3c3a63a844e
Michael Roop <michael@roop.ca>
2012-06-28 17:07:31 -0300

    Created Log wrappers
    Static class and tools that can be used to add loging to a project
    without comitting to one logging mechanism.  It supports basic verbosity
    level setting.  You hook in another loging system by subcribing to the
    log message event.

---
+++ TestCases/LogUtilsTests/LogTests.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using LogUtils;
+using ChkUtils.ErrObjects;
+using System.Threading;
+
+namespace TestCases.LogUtilsTests {
+
+    [TestFixture]
+    public class LogTests {
+
+        #region Data
+
+        MsgLevel currentLevel = MsgLevel.Off;
+        ErrReport currentReport = new ErrReport();
+        ConsoleWriter consoleWriter = new ConsoleWriter();
+
+        #endregion
+
+        #region Setup 
+
+        public LogTests() {
+            Log.OnLogMsgEvent += new LogingMsgEventDelegate(Log_OnLogMsgEvent);
+
+        }
+
+        void Log_OnLogMsgEvent(MsgLevel level, ErrReport errReport) {
+            this.currentLevel = level;
+            this.currentReport = errReport;
+        }
+
+
+        [SetUp]
+        public void Setup() {
+            LogUtils.Log.SetVerbosity(MsgLevel.Info);
+            LogUtils.Log.SetMsgNumberThreshold(1);
+            this.consoleWriter.StartLogging();
+
+            this.currentLevel = MsgLevel.Off;
+            this.currentReport = new ErrReport();
+        }
+        
+        #endregion
+
+        #region Valid Calls
+
+        [Test]
+        public void Info_Valid() {
+            Log.Info("LogTests", "Info_Valid", "This is my message");
+            this.CheckLogValues(MsgLevel.Info, 0, "Info_Valid", "This is my message", "");
+        }
+
+        [Test]
+        public void Debug_Valid() {
+            Log.Debug("LogTests", "Debug_Valid", "This is my message");
+            this.CheckLogValues(MsgLevel.Debug, 0, "Debug_Valid", "This is my message", "");
+        }
+
+        [Test]
+        public void Warning_Valid() {
+            Log.Warning(12345, "This is my warning string");
+            this.CheckLogValues(MsgLevel.Warning, 12345, "Warning_Valid", "This is my warning string", "");
+        }
+
+        [Test]
+        public void Error_Valid() {
+            Log.Error(8744, "This is my error string");
+            this.CheckLogValues(MsgLevel.Error, 8744, "Error_Valid", "This is my error string", "");
+        }
+
+        [Test]
+        public void Critical_Valid() {
+            Log.Critical(8989, "This is my critical error string");
+            this.CheckLogValues(MsgLevel.Critical, 8989, "Critical_Valid", "This is my critical error string", "");
+        }
+
+        [Test]
+        public void Exception_Valid() {
+            try {
+                throw new Exception("Blah blah exception");
+            }
+            catch (Exception e) {
+                Log.Exception(1000, "This is my exception error string", e);
+                this.CheckLogValues(MsgLevel.Exception, 1000, "Exception_Valid", "This is my exception error string", "Blah blah exception");
+            }
+        }
+
+        #endregion
+
+        #region Level Restricted Verbosity Calls
+
+        [Test]
+        public void Info_Restricted() {
+            Log.SetVerbosity(MsgLevel.Debug);
+            Log.Info("LogTests", "Info_Valid", "This is my message");
+            this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+
+            Log.SetVerbosity(MsgLevel.Info);
+            this.Info_Valid();
+        }
+
+        [Test]
+        public void Debug_Restricted() {
+            Log.SetVerbosity(MsgLevel.Warning);
+            Log.Debug("LogTests", "Debug_Valid", "This is my message");
+            this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+            
+            Log.SetVerbosity(MsgLevel.Debug);
+            this.Debug_Valid();
+        }
+
+        [Test]
+        public void Warning_Restricted() {
+            Log.SetVerbosity(MsgLevel.Error);
+            Log.Warning(1233, "This is my message");
+            this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+
+            Log.SetVerbosity(MsgLevel.Warning);
+            this.Warning_Valid();
+        }
+
+        [Test]
+        public void Error_Restricted() {
+            Log.SetVerbosity(MsgLevel.Critical);
+            Log.Error(1233, "This is my message");
+            this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+
+            Log.SetVerbosity(MsgLevel.Error);
+            this.Error_Valid();
+        }
+
+        [Test]
+        public void Critical_Restricted() {
+            Log.SetVerbosity(MsgLevel.Exception);
+            Log.Critical(1233, "This is my message");
+            this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+
+            Log.SetVerbosity(MsgLevel.Critical);
+            this.Critical_Valid();
+        }
+
+        [Test]
+        public void Exception_Restricted() {
+            try {
+                Log.SetVerbosity(MsgLevel.Off);
+                throw new Exception("Blah blah exception");
+            }
+            catch (Exception e) {
+                Log.Exception(1000, "This is my exception error string", e);
+                this.CheckLogValues(MsgLevel.Off, 0, "", "", "", "");
+
+
+                //this.CheckLogValues(MsgLevel.Exception, 1000, "Exception_Valid", "This is my exception error string", "Blah blah exception");
+            }
+
+            Log.SetVerbosity(MsgLevel.Exception);
+            this.Exception_Valid();
+
+        }
+
+        #endregion
+
+
+
+        #region Private Methods
+
+        public void CheckLogValues(MsgLevel level, int code, string atMethod, string msg, string stack) {
+            this.CheckLogValues(level, code, "LogTests", atMethod, msg, stack);
+        }
+
+        public void CheckLogValues(MsgLevel level, int code, string atClass, string atMethod, string msg, string stack) {
+            Thread.Sleep(250);
+            Assert.AreEqual(level, this.currentLevel, "Level Mismatch");
+            Assert.AreEqual(code, this.currentReport.Code, "Error Code Mismatch");
+            Assert.AreEqual(atClass, this.currentReport.AtClass, "Class Name Mismatch");
+            Assert.AreEqual(atMethod, this.currentReport.AtMethod, "Method Name Mismatch");
+            Assert.AreEqual(msg, this.currentReport.Msg, "Message Mismatch");
+
+            if (stack.Length == 0) {
+                Assert.AreEqual("", this.currentReport.StackTrace, "Stack trace supposed to be empty");
+            }
+            else {
+                Assert.IsTrue(this.currentReport.StackTrace.Contains(stack), String.Format("Stack does not contain '{0}' - {1}", stack, this.currentReport.StackTrace));
+            }
+
+        }
+
+        #endregion
+
+        
+
+    }
+}
---
+++ TestCases/LogUtilsTests/MsgLevelTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using LogUtils;
+
+namespace TestCases.LogUtilsTests {
+
+    [TestFixture]
+    public class MsgLevelTests {
+
+        #region ShortName extension
+
+        [Test]
+        public void ShortName_Info() {
+            Assert.AreEqual("I", MsgLevel.Info.ShortName());
+        }
+
+        [Test]
+        public void ShortName_Debug() {
+            Assert.AreEqual("D", MsgLevel.Debug.ShortName());
+        }
+
+        [Test]
+        public void ShortName_Error() {
+            Assert.AreEqual("E", MsgLevel.Error.ShortName());
+        }
+
+        [Test]
+        public void ShortName_Warning() {
+            Assert.AreEqual("W", MsgLevel.Warning.ShortName());
+        }
+
+        [Test]
+        public void ShortName_Critical() {
+            Assert.AreEqual("C", MsgLevel.Critical.ShortName());
+        }
+
+        [Test]
+        public void ShortName_Exception() {
+            Assert.AreEqual("X", MsgLevel.Exception.ShortName());
+        }
+
+        #endregion
+
+        #region GreaterOrEqual extension
+
+        [Test]
+        public void GreaterOrEqual_Debug_Info() {
+            Assert.IsTrue(MsgLevel.Debug.GreaterOrEqual(MsgLevel.Info));
+        }
+
+        [Test]
+        public void GreaterOrEqual_Debug_Debug() {
+            Assert.IsTrue(MsgLevel.Debug.GreaterOrEqual(MsgLevel.Debug));
+        }
+
+        [Test]
+        public void GreaterOrEqual_Debug_Warning() {
+            Assert.IsFalse(MsgLevel.Debug.GreaterOrEqual(MsgLevel.Warning));
+        }
+
+
+        #endregion
+    }
+}
