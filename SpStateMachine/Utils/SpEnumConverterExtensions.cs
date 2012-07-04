00b778c1eeced2a963a4b7ac99ebfba3ce41e8dd
Michael Roop <michael@roop.ca>
2012-07-03 23:13:45 -0300

    State machine Enum conversion extensions
    Initial checkin

---
+++ SpStateMachine/Utils/SpEnumConverterExtensions.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ChkUtils;
+
+namespace SpStateMachine.Utils {
+
+    public static class SpEnumConverterExtensions {
+
+        public static int ToInt(this Enum value) {
+            // TODO - put in checking
+            return Convert.ToInt32(value);
+        }
+
+
+        // Force non nullable
+        public static T ToEnum<T>(this int value) where T : struct {
+            T enumType = default(T);
+            T ret = 
+            WrapErr.ToErrorReportException(9999,
+                () => { return String.Format(
+                    "Enum Conversion Failed Attempting to Convert to Type '{0}' with Value '{1}'", 
+                    enumType.GetType().Name, value); }, 
+                () => {
+                    // This will throw on non enum but not out of range
+                    ret = (T)Enum.Parse(typeof(T), value.ToString());
+                    Console.WriteLine(ret.ToString());
+
+                    //WrapErr.ChkTrue(Enum.IsDefined(typeof(T), ret), 9999, () => {
+                    //    return String.Format(
+                    //        "Enum Conversion Out of Range Attempting to Convert to Type '{0}' with Value '{1}'", 
+                    //        enumType.GetType().Name, value);
+                    //});
+
+
+                    return ret;
+                    //id = (T)Enum.Parse(typeof(T), value.ToString());
+
+                    ////WrapErr.ToErrReport(9999, () => { return String.Format(""); }, () => {
+                    ////    id = (MyEventType)Enum.Parse(typeof(MyEventType), value.ToString());
+                    ////});
+                    //return id;
+                });
+
+            WrapErr.ChkTrue(Enum.IsDefined(typeof(T), ret), 9999, () => {
+                return String.Format(
+                    "Enum Conversion Out of Range Attempting to Convert to Type '{0}' with Value '{1}'",
+                    enumType.GetType().Name, value);
+            });
+            return ret;
+
+
+        }
+    }
+}
---
+++ TestCases/SpStateMachineTests/TestImplementations/SpEnumConverterTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using TestCases.TestToolSet;
+using SpStateMachine.Utils;
+
+namespace TestCases.SpStateMachineTests.TestImplementations {
+
+    enum TestEnum {
+        Gnarly,
+        Farly,
+        Whoopee,
+        Splat,
+    }
+
+
+    [TestFixture]
+    public class SpEnumConverterTests {
+
+        #region Data
+
+        HelperLogReader logReader = new HelperLogReader();
+
+        #endregion
+
+        #region Setup
+
+        [SetUp]
+        public void TestSetup() {
+            this.logReader.StartLogging();
+        }
+
+        [TearDown]
+        public void TestTeardown() {
+            this.logReader.StopLogging();
+            this.logReader.Clear();
+        }
+
+        #endregion
+
+        #region ToEnum
+
+        [Test]
+        public void _9999_ToEnum_DirectCall_UsingNonEnum() {
+            TestHelpers.CatchExpected(9999, "SpEnumConverterExtensions", "ToEnum", "Enum Conversion Failed Attempting to Convert to Type 'Int32' with Value '10'", () => {
+                SpEnumConverterExtensions.ToEnum<int>(10);
+            });
+
+            //Error Converting int '10' to Enum Type 'Int32'
+        }
+        
+        [Test]
+        public void _9999_ToEnum_Extension_UsingNonEnum() {
+            TestHelpers.CatchExpected(9999, "SpEnumConverterExtensions", "ToEnum", "Enum Conversion Failed Attempting to Convert to Type 'Int32' with Value '10'", () => {
+                int id = 10;
+                id.ToEnum<int>();
+            });
+        }
+
+
+        [Test]
+        public void _9999_ToEnum_DirectCall_OutOfRange() {
+            TestHelpers.CatchExpected(9999, "SpEnumConverterExtensions", "ToEnum", "Enum Conversion Out of Range Attempting to Convert to Type 'TestEnum' with Value '15'", () => {
+                SpEnumConverterExtensions.ToEnum<TestEnum>(15);
+            });
+        }
+
+
+        [Test]
+        public void _9999_ToEnum_Extension_OutOfRange() {
+            TestHelpers.CatchExpected(9999, "SpEnumConverterExtensions", "ToEnum", "Enum Conversion Out of Range Attempting to Convert to Type 'TestEnum' with Value '35'", () => {
+                int id = 35;
+                id.ToEnum<TestEnum>();
+            });
+        }
+
+
+        [Test]
+        public void _0_ToEnum_Extension_Good() {
+            TestHelpers.CatchUnexpected(() => {
+                int id = 2;
+                id.ToEnum<TestEnum>();
+            });
+        }
+
+
+
+
+//            return WrapErr.ToErrorReportException(9999, "Error Converting from Int to Enum", () => {
+
+
+        #endregion
+
+
+
+    }
+}
